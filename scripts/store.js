b12476e18fc14ada1eec05368044201bcd0fe28d
thaborak <thaborak@gmail.com>
2018-02-28 01:24:11 -0500

    added a link for more from the channel that each video came from

--- scripts/store.js
+++ scripts/store.js
@@ -10,7 +10,8 @@ const Store = function () {
       return {
         id: item.id.videoId, 
         title:  item.snippet.title, 
-        thumbnail:  item.snippet.thumbnails.high
+        thumbnail:  item.snippet.thumbnails.high,
+        channelId: item.snippet.channelId
       };
     });
     return videos;
@@ -40,6 +41,7 @@ const Store = function () {
       <div class="js-item-element" data-item-id="${videos.id}">
         <h1>${videos.title}</h1>
         <a href="https://www.youtube.com/watch?v=${videos.id}" data-lity><img src="${videos.thumbnail.url}" ></img></a>
+        <h2><a href="https://www.youtube.com/channel/${videos.channelId}">More from this Channel</a></h2>
       </div>`;
     return videoTitle;
   };
